73ef363a39a3ce6463155259a413f4f0ac4ed4e4
Srodaa <david030719@gmail.com>
2025-08-05 22:06:30 +0200

    set MySQL session timezone in PDO connection

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -10,6 +10,7 @@ class Database {
         $dsn = "mysql:host={$config['dbhost']};port={$config['dbport']};dbname={$config['dbname']};charset=utf8";
         $this->pdo = new PDO($dsn, $config['dbusername'], $config['dbpassword']);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo->exec("SET time_zone = 'Europe/Budapest'");
     }
 
     public function getConnection() {
